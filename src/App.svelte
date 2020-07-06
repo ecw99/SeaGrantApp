6497393a6a0070a9abd4c1856337184fb3820780
ecw99 <62108190+ecw99@users.noreply.github.com>
2020-07-06 10:23:13 -0400

    Added routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,22 @@
+<!-- App.svelte -->
 <script>
-	export let name;
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import Analysis from "./routes/Analysis.svelte";
+  import Nav from "./routes/Nav.svelte";
+
+  export let url = "";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Router url="{url}">
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="Nav">Nav</Link>
+    <Link to="Analysis">Analysis</Link>
+  </nav>
+  <div>
+    <Route path="Nav" component="{Nav}" />
+    <Route path="Analysis" component="{Analysis}" />
+    <Route path="/"><Home /></Route>
+  </div>
+</Router>
